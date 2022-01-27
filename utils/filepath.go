acc97f9c32bf691a9f52dcdda5a4bee117f29fbf
Dirk Arnez <smalldirkalex@gmail.com>
2022-01-27 16:27:07 +0800

    - update utils/filepath.go

--- utils/filepath.go
+++ utils/filepath.go
@@ -8,15 +8,25 @@ func GetFileName(path string) string {
 	return path[strings.LastIndex(path, "/")+1:]
 }
 
-// valid: `P:\testing`, `P:\testing\`
-// returning `testing`
+// fmt.Println(GetFolderName(`P`))           //P
+// fmt.Println(GetFolderName(`P:`))          //P
+// fmt.Println(GetFolderName(`P:\`))         //P
+// fmt.Println(GetFolderName(`P:\testing`))  //testing
+// fmt.Println(GetFolderName(`P:\testing\`)) //testing
 func GetFolderName(input string) string {
+	var folderName string
 	lastIndex := strings.LastIndex(input, `\`)
 	length := len(input)
 	if lastIndex+1 == length {
 		lastIndex = strings.LastIndex(input[0:length-1], `\`)
-		return input[lastIndex+1 : length-1]
+		folderName = input[lastIndex+1 : length-1]
 	} else {
-		return input[lastIndex+1 : length]
+		folderName = input[lastIndex+1 : length]
+	}
+
+	if folderName[len(folderName)-1:] == ":" {
+		return folderName[0 : len(folderName)-1]
+	} else {
+		return folderName
 	}
 }
