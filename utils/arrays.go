9479eb8333714ccec119da83d733fbe701bb41ad
Dirk Arnez <smalldirkalex@gmail.com>
2022-08-06 23:14:07 +0800

    - add utils/arrays.go

---
+++ utils/arrays.go
@@ -0,0 +1,6 @@
+package utils
+
+
+// 	listOfRepos = append(listOfRepos, "")  // Making space for the new element
+// 	copy(listOfRepos[1:], listOfRepos[0:]) // Shifting elements
+// 	listOfRepos[0] = repoName              // Copying/inserting the value
